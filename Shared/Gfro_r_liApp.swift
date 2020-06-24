06b4e2d2f982c91da3f3cbea5cd2291532dddbef
Nikals Liechti <nliechti@nliechti.ch>
2020-06-24 10:37:12 +0200

    Initial Commit

---
+++ Shared/Gfro_r_liApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gfro_r_liApp.swift
+//  Shared
+//
+//  Created by Niklas Liechti on 24.06.20.
+//
+
+import SwiftUI
+
+@main
+struct Gfro_r_liApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
