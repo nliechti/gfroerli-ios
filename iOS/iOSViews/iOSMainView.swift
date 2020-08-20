a92aae6a8f04b43de82ea6655f8d48f98ee4859e
Nikals Liechti <nliechti@nliechti.ch>
2020-08-20 16:50:12 +0200

    Add first map view

--- iOS/iOSViews/iOSMainView.swift
+++ iOS/iOSViews/iOSMainView.swift
@@ -6,10 +6,35 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct iOSMainView: View {
+    
+    @State var mapState = MKCoordinateRegion(center: CLLocationCoordinate2DMake(47.226015,  8.734503), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta:0.5))
+    @ObservedObject var pins = SensorViewModel()
+    
+    init() {
+        MKMapView.appearance().mapType = .satelliteFlyover
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Map (coordinateRegion: $mapState, annotationItems: pins.sensorArray, annotationContent: { pin in
+                MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: pin.latitude!, longitude: pin.longitude!), content: {
+                    Text(String(format: "%.1f", pin.last_measurement!.temperature!))
+                        .minimumScaleFactor(0.3)
+                        .lineLimit(1)
+                        .frame(width: 40, height: 40)
+                        .background(Color.blue)
+                        .cornerRadius(90)
+                
+                    
+                })
+            })
+            
+            //                .mapSyle(.satelliteFlyover)
+            .ignoresSafeArea()
+        }
     }
 }
 
