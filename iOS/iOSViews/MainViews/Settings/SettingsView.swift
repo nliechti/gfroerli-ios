20a54cc1fc3823b90aa51c95844c3745c69fd9b6
Marc Kramer <marc.kramer@uzh.ch>
2020-10-12 20:23:59 +0200

    Added reset button to settings

--- iOS/iOSViews/MainViews/Settings/SettingsView.swift
+++ iOS/iOSViews/MainViews/Settings/SettingsView.swift
@@ -6,9 +6,11 @@
 //
 
 import SwiftUI
+import WidgetKit
 
 struct SettingsView: View {
     @State var activePath: String?
+    @State var alertShowing = false
     @Binding var loadingState: loadingState
     @ObservedObject var sensorsVm : SensorListViewModel
     var body: some View {
@@ -18,18 +20,38 @@ struct SettingsView: View {
                     Section(header:Text("General")){
                         NavigationLink(destination: WidgetSettingsView(sensors: sensorsVm, loadingState: $loadingState), tag: "widgetSettings", selection: $activePath ,label: {Text("Widget Settings")})
                     }
-                    Section(header:Text("Other")){
+                    Section(header:Text("Web")){
                         Link("Privacy Policy", destination: URL(string: "https://xn--gfrr-7qa.li/about")!)
                         
                         Link("Gfrör.li Website", destination: URL(string: "https://xn--gfrr-7qa.li/")!)
 
                         Link("CoreDump Website", destination: URL(string: "https://www.coredump.ch/")!)
-                        Text("Version: \(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "fail")").foregroundColor(.gray)
+                        
+                    }
+                    Section(header: Text("Other"),footer:Text("Version: \(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "fail")").foregroundColor(.gray)){
+                        Button(action: {alertShowing=true}, label: {
+                            Text("Reset App").foregroundColor(.red)
+                        }).alert(isPresented: $alertShowing, content: {
+                            Alert(
+                              title: Text("Are you sure?"),
+                              message: Text("Do you want to reset the app?"),
+                              primaryButton: .destructive(Text("Reset"), action: resetContent),
+                                secondaryButton: .cancel(Text("Cancel"), action: {})
+                            )
+                        })
+                        
                     }
                 }
             }.navigationTitle("Settings")
             .background(Color.gray.opacity(0.001))
         }
+    }
+    
+    func resetContent(){
+        UserDefaults(suiteName: "group.ch.gfroerli")?.set([], forKey: "favoritesIDs")
+        UserDefaults(suiteName: "group.ch.gfroerli")?.set(0, forKey: "widgetSensorID")
+        WidgetCenter.shared.reloadAllTimelines()
+
     }
 }
 
